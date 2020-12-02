0459f891aa46d06dade3202a0e02aab592c721a9
xieqiupeng <xieqiupeng@gmail.com>
2020-12-03 07:22:09 +0800

    update

--- dependency.py
+++ dependency.py
@@ -14,16 +14,45 @@ ln -sf /Volumes/Project/Enum/Enum-Technology/大数据  $Path/大数据
 ln -sf /Volumes/Project/Enum/Enum-Technology/AI  $Path/AI
 ln -sf /Volumes/Project/Enum/Enum-Technology/Test  $Path/Test
 
+ln -sf /Volumes/Project/Enum/Enum-Java  $Path/Enum-Java
+ln -sf /Volumes/Project/Enum/Enum-BingFa  $Path/Enum-BingFa
+ln -sf /Volumes/Project/Rep/DataBase  $Path/DataBase
+ln -sf /Volumes/Project/Rep/Network  $Path/Network
+ln -sf /Volumes/Project/Rep/Java  $Path/Java
+ln -sf /Volumes/Project/Rep/BingFa  $Path/BingFa
+ln -sf /Volumes/Project/Rep/DataAlgorithm  $Path/DataAlgorithm
+ln -sf /Volumes/Project/Rep/NLP  $Path/NLP
+ln -sf /Volumes/Project/Rep/Server  $Path/Server
+ln -sf /Volumes/Project/Enum/Enum-Coder/Knowledge-Mapping  $Path/Knowledge-Mapping
+ln -sf /Volumes/Project/Enum/Enum-Coder/DataWarehouse  $Path/DataWarehouse
+ln -sf /Volumes/Project/Enum/Enum-BingFa/事务  $Path/事务
+ln -sf /Volumes/Project/Enum/Enum-BingFa/Lock  $Path/Lock
+ln -sf /Volumes/Project/Enum/Enum-Java/JVM  $Path/JVM
+ln -sf /Volumes/Project/Enum/Enum-DataBase/Index  $Path/Index
+ln -sf /Volumes/Project/Enum/Enum-DataBase/SQL  $Path/SQL
+ln -sf /Volumes/Project/Enum/Enum-BigData/Elasticsearch  $Path/Elasticsearch
+ln -sf /Volumes/Project/Instance/Instance-DataBase/MySQL  $Path/MySQL
+ln -sf /Volumes/Project/Instance/Instance-Server/VPN  $Path/VPN
+
+
 # Root
 Path=/Volumes/Project/List/List-Technology-Enum
-ln -sf /Volumes/Project/Enum/Enum-Technology/规范  $Path/规范
-ln -sf /Volumes/Project/Enum/Enum-Technology/网络和数据通信  $Path/网络和数据通信
-ln -sf /Volumes/Project/Enum/Enum-Technology/DataAlgorithm  $Path/数据结构和算法
-ln -sf /Volumes/Project/Enum/Enum-Technology/Java  $Path/Java
-ln -sf /Volumes/Project/Enum/Enum-Technology/资源  $Path/资源
-ln -sf /Volumes/Project/Enum/Enum-Technology/Web前端  $Path/Web前端
-ln -sf /Volumes/Project/Enum/Enum-Technology/DataBase  $Path/DataBase
-ln -sf /Volumes/Project/Enum/Enum-Technology/Android  $Path/Android
-ln -sf /Volumes/Project/Enum/Enum-Technology/大数据  $Path/大数据
-ln -sf /Volumes/Project/Enum/Enum-Technology/AI  $Path/AI
-ln -sf /Volumes/Project/Enum/Enum-Technology/Test  $Path/Test
+ln -sf /Volumes/Project/Enum/Enum-Java  $Path/Enum-Java
+ln -sf /Volumes/Project/Enum/Enum-BingFa  $Path/Enum-BingFa
+ln -sf /Volumes/Project/Rep/DataBase  $Path/DataBase
+ln -sf /Volumes/Project/Rep/Network  $Path/Network
+ln -sf /Volumes/Project/Rep/Java  $Path/Java
+ln -sf /Volumes/Project/Rep/BingFa  $Path/BingFa
+ln -sf /Volumes/Project/Rep/DataAlgorithm  $Path/DataAlgorithm
+ln -sf /Volumes/Project/Rep/NLP  $Path/NLP
+ln -sf /Volumes/Project/Rep/Server  $Path/Server
+ln -sf /Volumes/Project/Enum/Enum-Coder/Knowledge-Mapping  $Path/Knowledge-Mapping
+ln -sf /Volumes/Project/Enum/Enum-Coder/DataWarehouse  $Path/DataWarehouse
+ln -sf /Volumes/Project/Enum/Enum-BingFa/事务  $Path/事务
+ln -sf /Volumes/Project/Enum/Enum-BingFa/Lock  $Path/Lock
+ln -sf /Volumes/Project/Enum/Enum-Java/JVM  $Path/JVM
+ln -sf /Volumes/Project/Enum/Enum-DataBase/Index  $Path/Index
+ln -sf /Volumes/Project/Enum/Enum-DataBase/SQL  $Path/SQL
+ln -sf /Volumes/Project/Enum/Enum-BigData/Elasticsearch  $Path/Elasticsearch
+ln -sf /Volumes/Project/Instance/Instance-DataBase/MySQL  $Path/MySQL
+ln -sf /Volumes/Project/Instance/Instance-Server/VPN  $Path/VPN
