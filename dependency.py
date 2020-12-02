b8a0a61ff6b286e1ee2bfd82adeb58a09582e990
xieqiupeng <xieqiupeng@gmail.com>
2020-12-03 07:20:04 +0800

    udpate

---
+++ dependency.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+# Dependency
+ln -sf /Volumes/Project/Enum/Enum-Technology/规范  $Path/规范
+ln -sf /Volumes/Project/Enum/Enum-Technology/网络和数据通信  $Path/网络和数据通信
+ln -sf /Volumes/Project/Enum/Enum-Technology/DataAlgorithm  $Path/数据结构和算法
+ln -sf /Volumes/Project/Enum/Enum-Technology/Java  $Path/Java
+ln -sf /Volumes/Project/Enum/Enum-Technology/资源  $Path/资源
+ln -sf /Volumes/Project/Enum/Enum-Technology/Web前端  $Path/Web前端
+ln -sf /Volumes/Project/Enum/Enum-Technology/DataBase  $Path/DataBase
+ln -sf /Volumes/Project/Enum/Enum-Technology/Android  $Path/Android
+ln -sf /Volumes/Project/Enum/Enum-Technology/大数据  $Path/大数据
+ln -sf /Volumes/Project/Enum/Enum-Technology/AI  $Path/AI
+ln -sf /Volumes/Project/Enum/Enum-Technology/Test  $Path/Test
+
+# Root
+Path=/Volumes/Project/List/List-Technology-Enum
+ln -sf /Volumes/Project/Enum/Enum-Technology/规范  $Path/规范
+ln -sf /Volumes/Project/Enum/Enum-Technology/网络和数据通信  $Path/网络和数据通信
+ln -sf /Volumes/Project/Enum/Enum-Technology/DataAlgorithm  $Path/数据结构和算法
+ln -sf /Volumes/Project/Enum/Enum-Technology/Java  $Path/Java
+ln -sf /Volumes/Project/Enum/Enum-Technology/资源  $Path/资源
+ln -sf /Volumes/Project/Enum/Enum-Technology/Web前端  $Path/Web前端
+ln -sf /Volumes/Project/Enum/Enum-Technology/DataBase  $Path/DataBase
+ln -sf /Volumes/Project/Enum/Enum-Technology/Android  $Path/Android
+ln -sf /Volumes/Project/Enum/Enum-Technology/大数据  $Path/大数据
+ln -sf /Volumes/Project/Enum/Enum-Technology/AI  $Path/AI
+ln -sf /Volumes/Project/Enum/Enum-Technology/Test  $Path/Test
